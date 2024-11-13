3c8f6a1daa6a4a1acada2bd6d1bf0b5627e998d6
VictorB <anzac62@gmail.com>
2024-11-13 16:43:30 -0700

    Finished learning 2

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,16 +1,20 @@
 using System;
+// A code template for the category of things known as Job. The
+// responsibility of a Job is to hold and display job information.
 
 // Responsibility: Keep track of the company, job title, start year, and end year.
 // Behavior: Display the job information in the format "Job Title (Company) StartYear-EndYear".
 
 public class Job
 {
-    // variables
+    // member variables
+    // The C# convention is to start member variables with an underscore _
     public string _company = "";
     public string _jobTitle = "";
     public int _startYear = 0;
     public int _endYear = 0;
 
+    // A method that displays a person's list of jobs 
     public Job()
     {
     }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,16 +1,23 @@
+// A code template for the category of things known as Resume. The
+// responsibility of a Resume is to hold and display a person's job information.
+
 // Responsibility: Keep track of the person's name and a list of their jobs.
 // Displays the resume, which shows the name first, followed by displaying each one of the jobs.
+
 public class Resume
 {
-    // variables
+    // member variables
+    // The C# convention is to start member variables with an underscore _
     public string _name = "";
     public List<Job> _jobs = new List<Job>();
 
+    // A special method, called a constructor that is invoked using the  
+    // new keyword followed by the class name and parentheses.
     public Resume()
     {
     }
 
-    // Add a Display method
+    // A method that displays a person's resume 
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
